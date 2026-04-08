e29be741f490d6c24ab0b5138850632d1fb861d8
Matti Eskelinen <matti.eskelinen@amd.com>
2026-04-08 02:43:09 -0400

    Add conditional to select the FuseAQuant pipeline

--- projects/composablekernel/test/ck_tile/gemm_block_scale/test_gemm_quant_fixtures.hpp
+++ projects/composablekernel/test/ck_tile/gemm_block_scale/test_gemm_quant_fixtures.hpp
@@ -1021,6 +1021,7 @@ class TestCkTileGemmABQuant : public TestCkTileGemmQuantBase<Tuple, TestCkTileGe
     static constexpr auto QuantType        = Base::QuantType;
     static constexpr auto PreshuffleB      = Base::PreshuffleB;
     static constexpr auto TiledMMAPermuteN = Base::TiledMMAPermuteN;
+    static constexpr auto FuseAQuant       = Base::FuseAQuant;
 
     protected:
     void SetUpQuantTypeSpecific() {}
@@ -1288,9 +1289,13 @@ class TestCkTileGemmABQuant : public TestCkTileGemmQuantBase<Tuple, TestCkTileGe
             using GemmPipeline = std::conditional_t<
                 eight_waves,
                 ck_tile::ABQuantGemmPipelineAgBgCrEightWaves<PipelineProblem>,
-                std::conditional_t<PreshuffleB,
-                                   ck_tile::WPABQuantBPipelineAgBgCrV2<PipelineProblem>,
-                                   ck_tile::ABQuantGemmPipelineAgBgCrCompV3<PipelineProblem>>>;
+                std::conditional_t<
+                    PreshuffleB,
+                    ck_tile::WPABQuantBPipelineAgBgCrV2<PipelineProblem>,
+                    std::conditional_t<
+                        FuseAQuant,
+                        ck_tile::FusedAQuantBQuantGemmPipelineAgBgCrCompV3<PipelineProblem>,
+                        ck_tile::ABQuantGemmPipelineAgBgCrCompV3<PipelineProblem>>>>;
 
             using GemmEpilogue = ck_tile::CShuffleEpilogue<
                 ck_tile::CShuffleEpilogueProblem<typename PipelineProblem::ComputeDataType,
